978e566a336889422a249ebf8ee8617af919eb99
Joey Grable <hello@joeygrable.com>
2024-07-31 15:20:19 -0700

    added default eslint config

--- library.js
+++ library.js
@@ -1,13 +1,4 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    ecmaFeatures: { jsx: true },
-    sourceType: 'module',
-  },
-  globals: {
-    JSX: true,
-    NodeJS: true,
-  },
   extends: ['eslint:recommended', 'typescript-eslint:recommended'],
 };
--- solid.js
+++ solid.js
@@ -1,10 +1,14 @@
-module.exports = {
-  parser: '@typescript-eslint/parser',
-  files: ['**/*.{ts,tsx,mdx}'],
-  plugins: ['solid'],
-  extends: [
-    'eslint:recommended',
-    'typescript-eslint:recommended',
-    'plugin:solid/typescript',
-  ],
-};
+module.exports = [
+  {
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+    },
+    files: ['**/*.{ts,tsx,mdx}'],
+    plugins: ['solid'],
+    extends: [
+      'eslint:recommended',
+      'typescript-eslint:recommended',
+      'plugin:solid/typescript',
+    ],
+  },
+];
