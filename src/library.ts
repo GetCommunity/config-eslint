bc21f53c6435faf5279629c7cd72e89263eb086c
Joey Grable <hello@joeygrable.com>
2024-11-20 11:34:44 -0800

    updated deps

--- src/library.ts
+++ src/library.ts
@@ -1,10 +1,7 @@
-// @ts-check
-
 import eslint from '@eslint/js';
-import { Linter } from '@typescript-eslint/utils/ts-eslint';
 import tseslint from 'typescript-eslint';
 
-const gcLibEsConfig: Linter.ConfigType = tseslint.config(
+const gcLibEsConfig: ReturnType<typeof tseslint.config> = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
--- src/solid.ts
+++ src/solid.ts
@@ -1,10 +1,9 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import { Linter } from '@typescript-eslint/utils/ts-eslint';
 import tseslint from 'typescript-eslint';
 
-const gcSolidJsEsConfig: Linter.ConfigType = tseslint.config(
+const gcSolidJsEsConfig: ReturnType<typeof tseslint.config> = tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
