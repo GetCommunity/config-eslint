0eafc1c2f12872527471f696c4546b4c4a041267
Joey Grable <hello@joeygrable.com>
2025-07-21 11:22:04 -0700

    added solid-tanstack eslint

---
+++ src/solid-tanstack.ts
@@ -0,0 +1,59 @@
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import tsParser from "@typescript-eslint/parser"
+// @ts-expect-error - no types for eslint-plugin-tailwindcss
+import tailwindcss from "eslint-plugin-tailwindcss"
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
+
+export default [
+  {
+    ignores: ["**/*.config.*", "**/*.json", "**/lib/ui/*.tsx"]
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:solid/recommended",
+    "plugin:tailwindcss/recommended"
+  ),
+  {
+    plugins: {
+      tailwindcss
+    },
+    settings: {
+      tailwindcss: {
+        callees: ["cn", "cva"],
+        config: "tailwind.config.mjs"
+      }
+    },
+
+    rules: {
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_"
+        }
+      ],
+      "tailwindcss/classnames-order": "off",
+      "tailwindcss/no-custom-classname": "off"
+    }
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser
+    }
+  }
+]
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["src/library.ts", "src/typescript.ts", "src/solid.ts", "src/tailwind.ts"],
+  entry: [
+    "src/library.ts",
+    "src/typescript.ts",
+    "src/tailwind.ts",
+    "src/solid.ts",
+    "src/solid-tanstack.ts"
+  ],
   format: ["cjs", "esm"],
   dts: true,
   splitting: false,
