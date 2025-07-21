db7847afa5b76070f943ff00fc79b29759e5aacb
Joey Grable <hello@joeygrable.com>
2025-07-21 11:09:04 -0700

    added esm dirname filename support

--- src/tailwind.ts
+++ src/tailwind.ts
@@ -3,7 +3,11 @@ import { FlatCompat } from "@eslint/eslintrc"
 import eslint from "@eslint/js"
 // @ts-expect-error - no types for eslint-plugin-tailwindcss
 import tailwindcss from "eslint-plugin-tailwindcss"
+import path from "path"
+import { fileURLToPath } from "url"
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: eslint.configs.recommended,
--- src/typescript.ts
+++ src/typescript.ts
@@ -2,7 +2,11 @@
 import { FlatCompat } from "@eslint/eslintrc"
 import eslint from "@eslint/js"
 import tsParser from "@typescript-eslint/parser"
+import path from "path"
+import { fileURLToPath } from "url"
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: eslint.configs.recommended,
